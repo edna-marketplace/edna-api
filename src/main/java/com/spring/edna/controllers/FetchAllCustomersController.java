fecc61a7d609f3410483d3fc33d30c3d625a8b1b
matheusbarcc <matheusbarcon@gmail.com>
2025-01-03 19:49:59 -0300

    feat: add route to fetch all customers

--- src/main/java/com/spring/edna/controllers/FetchAllCustomersController.java
+++ src/main/java/com/spring/edna/controllers/FetchAllCustomersController.java
@@ -2,22 +2,23 @@ package com.spring.edna.controllers;
 
 import com.spring.edna.models.entities.Customer;
 import com.spring.edna.services.CreateCustomer;
+import com.spring.edna.services.FetchAllCustomers;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/customers")
-public class CreateCustomerController {
+public class FetchAllCustomersController {
 
     @Autowired
-    private CreateCustomer createCustomer;
+    private FetchAllCustomers fetchAllCustomers;
 
-    @PostMapping
-    public ResponseEntity<Void> createCustomer(@Valid @RequestBody Customer customer) {
-        createCustomer.execute(customer);
-
-        return ResponseEntity.ok().build();
+    @GetMapping
+    public List<Customer> fetchAllCustomers() {
+        return fetchAllCustomers.execute();
     }
 }
