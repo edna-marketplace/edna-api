21b934dc0f71fbac9ba6e674c75dcb77897ad86a
matheusbarcc <matheusbarcon@gmail.com>
2025-01-10 14:29:14 -0300

    refactor: add missing values on store details dto

--- src/main/java/com/spring/edna/models/dtos/StoreDetailsDTO.java
+++ src/main/java/com/spring/edna/models/dtos/StoreDetailsDTO.java
@@ -10,10 +10,13 @@ import java.util.List;
 @AllArgsConstructor
 public class StoreDetailsDTO {
     private String storeId;
+    private String bannerImageUrl;
+    private String profileImageUrl;
     private String storeName;
     private TargetCustomer targetCustomer;
     private Integer distanceToCustomerInMeters;
     private Double avgRating;
+    private boolean isFavorite;
     private String storeDescription;
     private String cnpj;
     private AddressDetailsDTO address;
--- src/main/java/com/spring/edna/models/mappers/StoreMapper.java
+++ src/main/java/com/spring/edna/models/mappers/StoreMapper.java
@@ -7,13 +7,15 @@ import com.spring.edna.utils.StoreRatingUtils;
 public class StoreMapper {
 
     public static StoreDetailsDTO toStoreDetailsDTO(Store store) {
-
         return new StoreDetailsDTO(
                 store.getId(),
+                "mock-banner-image-url", // Mock value; TODO: Create images logic
+                "mock-profile-image-url", // Mock value; TODO: Create images logic
                 store.getStoreName(),
                 store.getTargetCustomer(),
-                5000, // Mock value; TODO: Create a class and logic to calculate the distance
+                5000, // Mock value; TODO: Create logic to calculate the distance
                 StoreRatingUtils.calculateAverageRating(store.getRatings()),
+                false, // Mock value; TODO: Create logic with authenticated user id
                 store.getStoreDescription(),
                 store.getCnpj(),
                 AddressMapper.toAddressDetailsDTO(store.getAddress()),
