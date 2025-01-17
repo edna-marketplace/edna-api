bf2bdd68419a3942fc2c62058a89783134197bf4
Victorlapa3 <victorlapa0p@gmail.com>
2025-01-16 21:02:24 -0300

    feat: Modify CustomerDetailsDTO to include additional fields

--- src/main/java/com/spring/edna/models/dtos/CustomerDetailsDTO.java
+++ src/main/java/com/spring/edna/models/dtos/CustomerDetailsDTO.java
@@ -1,4 +1,16 @@
 package com.spring.edna.models.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 public class CustomerDetailsDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+    private String cpf;
 }
