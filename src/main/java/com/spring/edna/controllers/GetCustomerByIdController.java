cdf775b0f03bc863f89bbe9399c691c35e3203e3
Victorlapa3 <victorlapa0p@gmail.com>
2025-01-16 21:03:28 -0300

    feat: Add get customer by ID functionality

--- src/main/java/com/spring/edna/controllers/GetCustomerByIdController.java
+++ src/main/java/com/spring/edna/controllers/GetCustomerByIdController.java
@@ -1,4 +1,20 @@
 package com.spring.edna.controllers;
 
+import com.spring.edna.exception.EdnaException;
+import com.spring.edna.models.dtos.CustomerDetailsDTO;
+import com.spring.edna.services.GetCustomerById;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/customers")
 public class GetCustomerByIdController {
+
+    @Autowired
+    private GetCustomerById getCustomerById;
+
+    @GetMapping("/{customerId}")
+    public CustomerDetailsDTO getCustomerById(@PathVariable String customerId) throws EdnaException {
+        return getCustomerById.execute(customerId);
+    }
 }
