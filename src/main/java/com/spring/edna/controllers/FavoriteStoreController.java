8320723112c2a6cbd58c758f4e1910cf4ab8e5e4
Victorlapa3 <victorlapa0p@gmail.com>
2025-02-07 16:05:03 -0300

    feat: add route to favorite store

--- src/main/java/com/spring/edna/controllers/FavoriteStoreController.java
+++ src/main/java/com/spring/edna/controllers/FavoriteStoreController.java
@@ -1,4 +1,13 @@
 package com.spring.edna.controllers;
 
+import com.spring.edna.exception.EdnaException;
+import com.spring.edna.services.FavoriteStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/customers")
 public class FavoriteStoreController {
+
 }
