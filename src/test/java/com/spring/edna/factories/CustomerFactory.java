67169b58d82211280e83f7424d9119f6607ef370
Victorlapa3 <victorlapa0p@gmail.com>
2025-02-07 16:07:50 -0300

    test: add create customer tests

---
+++ src/test/java/com/spring/edna/factories/CustomerFactory.java
@@ -0,0 +1,20 @@
+package com.spring.edna.factories;
+
+import com.spring.edna.models.entities.Customer;
+import com.spring.edna.utils.CPFGenerator;
+import com.spring.edna.utils.NameGenerator;
+import com.spring.edna.utils.PhoneNumberGenerator;
+
+import java.util.UUID;
+
+public class CustomerFactory {
+    public static Customer create() {
+        Customer c = new Customer();
+        c.setName(NameGenerator.generateName());
+        c.setCpf(CPFGenerator.generateValidCPF());
+        c.setEmail(NameGenerator.generateName() + (UUID.randomUUID()) + "@gmail.com");
+        c.setPhone(PhoneNumberGenerator.generateValidPhoneNumber());
+
+        return c;
+    }
+}
