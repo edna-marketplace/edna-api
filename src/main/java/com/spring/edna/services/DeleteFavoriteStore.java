bc65694eef04e34f56bae68959721b704884ebc2
Victorlapa3 <victorlapa0p@gmail.com>
2025-02-13 21:00:05 -0300

    feat: add DeleteFavoriteStore service

--- src/main/java/com/spring/edna/services/DeleteFavoriteStore.java
+++ src/main/java/com/spring/edna/services/DeleteFavoriteStore.java
@@ -1,4 +1,35 @@
 package com.spring.edna.services;
 
+import com.spring.edna.exception.EdnaException;
+import com.spring.edna.models.entities.Customer;
+import com.spring.edna.models.entities.Store;
+import com.spring.edna.models.repositories.CustomerRepository;
+import com.spring.edna.models.repositories.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DeleteFavoriteStore {
+
+    @Autowired
+    private StoreRepository storeRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public HttpStatus execute(String customerId, String storeId) throws EdnaException {
+        Customer customer = customerRepository.findById(customerId).orElseThrow(() ->
+                new EdnaException("Customer not found", HttpStatus.BAD_REQUEST));
+        Store store = storeRepository.findById(storeId).orElseThrow(() ->
+                new EdnaException("Store not found", HttpStatus.BAD_REQUEST));
+
+        // Checks if the store is in the customer's favorites
+        if (customer.getFavoriteStores().contains(store)) {
+            customer.getFavoriteStores().remove(store);
+            customerRepository.save(customer);
+        } else {
+            throw new EdnaException("Store is not in the customer's favorite list", HttpStatus.BAD_REQUEST);
+        }
+        return HttpStatus.NO_CONTENT;
+    }
 }
