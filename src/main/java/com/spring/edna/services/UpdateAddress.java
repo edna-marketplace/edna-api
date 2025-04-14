6c0dc920db88ca2347318722dd44d051d17b9652
MATHEUS BARCELOS CONCEICAO <matheus.conceicao@alunos.sc.senac.br>
2025-04-14 09:43:26 -0300

    refactor: update route to update store address

--- src/main/java/com/spring/edna/services/UpdateAddress.java
+++ src/main/java/com/spring/edna/services/UpdateAddress.java
@@ -16,20 +16,25 @@ public class UpdateAddress {
     public HttpStatus execute(Address address, String storeId) throws EdnaException {
 
         Address addressWithSameCepAndNumber = addressRepository.findByCepAndNumber(address.getCep(), address.getNumber())
-                                                                .orElse(null);
+                .orElse(null);
 
-        if(addressWithSameCepAndNumber == null || addressWithSameCepAndNumber.getId().equals(address.getId())) {
+        if (addressWithSameCepAndNumber == null || addressWithSameCepAndNumber.getId().equals(address.getId())) {
 
-            Address addressInDatabase = addressRepository.findById(address.getId()).orElseThrow(() -> new EdnaException(
-                    "Address not found", HttpStatus.BAD_REQUEST)
-            );
+            Address addressInDB;
 
-            if(!addressInDatabase.getStore().getId().equals(storeId)) {
-                throw new EdnaException("You can only update the address from your store.", HttpStatus.BAD_REQUEST);
+            if (addressWithSameCepAndNumber != null) {
+                addressInDB = addressWithSameCepAndNumber;
+            } else {
+                addressInDB = addressRepository.findById(address.getId()).orElseThrow(() -> new EdnaException(
+                        "Address not found", HttpStatus.BAD_REQUEST)
+                );
+                if (!addressInDB.getStore().getId().equals(storeId)) {
+                    throw new EdnaException("You can only update the address from your store.", HttpStatus.BAD_REQUEST);
+                }
             }
 
-            address.setStore(addressInDatabase.getStore());
-            address.setCreatedAt(addressInDatabase.getCreatedAt());
+            address.setStore(addressInDB.getStore());
+            address.setCreatedAt(addressInDB.getCreatedAt());
 
             addressRepository.save(address);
         } else {
