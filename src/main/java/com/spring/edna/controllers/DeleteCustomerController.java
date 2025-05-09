8594449136bb7191be19fb93083a51ad7f4aa6ca
matheusbarcc <matheusbarcon@gmail.com>
2025-05-08 21:43:20 -0300

    refactor: delete customer route

--- src/main/java/com/spring/edna/controllers/DeleteCustomerController.java
+++ src/main/java/com/spring/edna/controllers/DeleteCustomerController.java
@@ -5,10 +5,8 @@ import com.spring.edna.exception.EdnaException;
 import com.spring.edna.models.entities.User;
 import com.spring.edna.services.DeleteCustomer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,16 +20,12 @@ public class DeleteCustomerController {
     @Autowired
     private AuthService authService;
 
-    @DeleteMapping("/{customerId}")
-    public ResponseEntity<Void> handle(@PathVariable String customerId) throws EdnaException {
+    @DeleteMapping()
+    public ResponseEntity<Void> handle() throws EdnaException {
         User subject = authService.getAuthenticatedUser();
 
-        if(subject.getId().equals(customerId)) {
-            deleteCustomer.execute(customerId);
+        deleteCustomer.execute(subject.getId());
 
-            return ResponseEntity.ok().build();
-        } else {
-            throw new EdnaException("You cant delete another customer's account.", HttpStatus.BAD_REQUEST);
-        }
+        return ResponseEntity.ok().build();
     }
 }
