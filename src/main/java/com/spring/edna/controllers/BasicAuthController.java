3d5847d1a3c3087fdf417cae1c31f71c97877554
matheusbarcc <matheusbarcon@gmail.com>
2025-06-28 11:51:38 -0300

    chore: add basic auth route for development

---
+++ src/main/java/com/spring/edna/controllers/BasicAuthController.java
@@ -0,0 +1,44 @@
+package com.spring.edna.controllers;
+
+import com.spring.edna.auth.AuthService;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public/basic-auth")
+public class BasicAuthController {
+
+
+    @Data
+    public static class SignInRequest {
+        private String email;
+        private String password;
+    }
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public String handle(@RequestBody SignInRequest credentials) {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                credentials.getEmail(),
+                credentials.getPassword()
+        );
+
+        Authentication authenticated = authenticationManager.authenticate(authentication);
+
+        String token = authService.authenticate(authenticated);
+
+        return token;
+    }
+}
