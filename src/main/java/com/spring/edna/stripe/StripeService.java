00be29fb3590a406bc34a01db3498f58c474f498
matheusbarcc <matheusbarcon@gmail.com>
2025-06-29 02:00:55 -0300

    fix: update refresh and return stripe urls

--- src/main/java/com/spring/edna/stripe/StripeService.java
+++ src/main/java/com/spring/edna/stripe/StripeService.java
@@ -43,8 +43,8 @@ public class StripeService {
     }
 
     public String createOnboardingLink(String email, String accountId) throws StripeException {
-        String refreshUrl = "http://localhost:3000/stripe-refresh";
-        String returnUrl = "http://localhost:3000/stripe-return";
+        String refreshUrl = "http://localhost:3000/signup/stripe-refresh";
+        String returnUrl = "http://localhost:3000/signup/stripe-return";
 
         AccountLinkCreateParams params = AccountLinkCreateParams.builder()
                 .setAccount(accountId)
